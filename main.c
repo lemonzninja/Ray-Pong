c007976efb9330c2a890c9ad6a21851905a4b68e
Holographic <budninja36@gmail.com>
2023-03-21 22:21:55 -0400

    #

--- main.c
+++ main.c
@@ -1,3 +1,21 @@
+/*
+    Pong clone made with raylib
+    
+    - Start menu with a start button and a title.
+    - When game has started A ball that bounces off the walls and the paddles.
+    - When game has started Two paddles that move up and down.
+    - When game has started Ai that moves the opponent paddle up and down.
+    - When game has started A score counter.
+    - When game has started A pause button.
+    - When game has started A restart button.
+    - A quit button.
+    - A Info button in the start menu.
+    - When game has started A sound effect when the ball hits the paddles.
+    - When game has started A sound effect when the ball hits the walls.
+    - When game has started A sound effect when the ball hits the score counter.
+*/
+
+
 #include "../../../src/raylib.h"
 
 #if defined(PLATFORM_WEB)
@@ -10,9 +28,16 @@ const int screenHeight = 450;
 // variable to hold the mouse position
 Vector2 mousePosition = {0, 0};
 
+// Title Variable
+char title[20] = "Ray Pong";
+
 // Start Button Variables and Struct
 Rectangle StartButtonRectangle = {0, 0, 0, 0}; // Button rectangle
-bool buttonClicked = false;      // True if the StartButtonRectangle is clicked
+bool StartButtonClicked = false;      // True if the StartButtonRectangle is clicked
+
+// info button variables
+Rectangle InfoButtonRectangle = {0, 0, 0, 0}; // Button rectangle
+bool InfoButtonClicked = false;      // True if the StartButtonRectangle is clicked
 
 typedef struct ButtonBox // A struct to hold the box data
 {
@@ -22,6 +47,9 @@ typedef struct ButtonBox // A struct to hold the box data
 } ButtonBox;
 ButtonBox StartButtonBox = {0};
 //----------------------------------------------------------------------------------
+
+
+ButtonBox InfoButtonBox = {0};
 
 // Ball Struct
 typedef struct Ball
@@ -55,7 +83,7 @@ int main()
         StartButtonRectangle is the rectangle that will be drawn on the screen
         StartButtonBox is the struct that holds the box data
     */
-    StartButtonRectangle.width = 50;
+    StartButtonRectangle.width = 100;
     StartButtonRectangle.height = 50;
     // Set the StartButtonRectangle position
     StartButtonRectangle.x = 350;
@@ -66,9 +94,28 @@ int main()
     // Set the box position
     StartButtonBox.position.x = screenWidth / 2 - StartButtonBox.size.x / 2;
     StartButtonBox.position.y = screenHeight / 2 - StartButtonBox.size.y / 2;
-    // Set the box color
-    StartButtonBox.color = RED;
     //--------------------------------------------------------------------------------------
+
+    /* 
+        The InfoButton variables
+        InfoButtonRectangle is the rectangle that will be drawn on the screen
+        InfoButtonBox is the struct that holds the box data
+        make the InfoButtonRectangle the same size as the StartButtonRectangle
+        make the InfoButtonBox just under the StartButtonBox
+    */
+    InfoButtonRectangle.width = 100;
+    InfoButtonRectangle.height = 50;
+    // Set the InfoButtonRectangle position
+    InfoButtonRectangle.x = 350;
+    InfoButtonRectangle.y = 300;
+    // Set the box size
+    InfoButtonBox.size.x = 50;
+    InfoButtonBox.size.y = 50;
+    // Set the box position
+    InfoButtonBox.position.x = screenWidth / 2 - InfoButtonBox.size.x / 2;
+    InfoButtonBox.position.y = screenHeight / 2 - InfoButtonBox.size.y / 2 + 100;
+    //--------------------------------------------------------------------------------------
+
 
     /*
         The ball variables
@@ -122,7 +169,7 @@ static void UpdateDrawFrame(void)
     // Check if the mouse is over the StartButtonRectangle
     if (CheckCollisionPointRec(mousePosition, StartButtonRectangle) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
     {
-        buttonClicked = true;
+        StartButtonClicked = true;
         DrawText("Clicked", 10, 10, 20, BLACK);
     }
     
@@ -138,8 +185,27 @@ static void UpdateDrawFrame(void)
         StartButtonBox.color = DARKGRAY;
     }
 
+    // Check if the mouse is over the InfoButtonRectangle
+    if (CheckCollisionPointRec(mousePosition, InfoButtonRectangle) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON))
+    {
+        InfoButtonClicked = true;
+    }
+    // Check if the mouse is over the InfoButtonRectangle and change the color of the box
+    if (CheckCollisionPointRec(mousePosition, InfoButtonRectangle))
+    {
+        // Set the box color to light gray
+        InfoButtonBox.color = LIGHTGRAY;
+    }
+    else
+    {   
+        // Set the box color to dark gray
+        InfoButtonBox.color = DARKGRAY;
+    }
+    
+
+
     // If the StartButtonRectangle is clicked, start the game
-    if (buttonClicked)
+    if (StartButtonClicked)
     {
         StartGame();
     }
@@ -151,11 +217,12 @@ static void UpdateDrawFrame(void)
     ClearBackground(RAYWHITE);
 
     // if game is started,hide the StartButtonRectangle and show the game
-    if (!buttonClicked)
+    if (!StartButtonClicked)
     {
 
         // Draw the Start Menu
         DrawStart();
+                       
     }
     else
     {
@@ -187,8 +254,16 @@ void DrawGame()
 // Draw the start StartButtonRectangle
 void DrawStart()
 {
+    // Draw the title text size 100 in the center of the screen above the StartButtonRectangle
+    DrawText(title, screenWidth / 2 - MeasureText(title, 100) / 2, screenHeight / 2 - 200, 100, BLACK);
+
     DrawRectangleRec(StartButtonRectangle, StartButtonBox.color);
-    DrawText("Start", StartButtonRectangle.x + 10, StartButtonRectangle.y + 10, 20, BLACK);
+    
+    // Draw the InfoButtonRectangle
+    DrawRectangleRec(InfoButtonRectangle, InfoButtonBox.color);
+
+    // Draw the text "Start" in the center of the StartButtonRectangle
+    DrawText("Start", screenWidth / 2 - MeasureText("Start", 20) / 2, screenHeight / 2 - 10, 20, BLACK);
 }
 
 void MoveBall()
